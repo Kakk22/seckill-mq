a4c3e3ce0603007c1bd7930e5d16c0480d0bc390
Kakk22 <1029839308@qq.com>
2021-03-22 22:51:55 +0800

    秒杀订单业务幂等

--- seckill-order-service/src/main/java/com/cyf/mq/listener/SeckillOrderLister.java
+++ seckill-order-service/src/main/java/com/cyf/mq/listener/SeckillOrderLister.java
@@ -35,7 +35,7 @@ public class SeckillOrderLister implements MessageListenerConcurrently {
     @Override
     public ConsumeConcurrentlyStatus consumeMessage(List<MessageExt> msgs, ConsumeConcurrentlyContext context) {
 
-        for (MessageExt ext: msgs) {
+        for (MessageExt ext : msgs) {
             String msgId = ext.getMsgId();
             int times = ext.getReconsumeTimes();
             String message = new String(ext.getBody());
@@ -47,13 +47,25 @@ public class SeckillOrderLister implements MessageListenerConcurrently {
             log.info("秒杀订单监听器解码消息,消息内容:{}", orderMsgProtocol.toString());
 
             //消息幂等:判断是否消费过
-            List<Order> list = orderService.list(new QueryWrapper<Order>().eq("order_sn", orderMsgProtocol.getOrderSn()));
+            String orderSn = orderMsgProtocol.getOrderSn();
+            List<Order> list = orderService.list(new QueryWrapper<Order>().eq("order_sn", orderSn));
             if (CollectionUtil.isNotEmpty(list)) {
                 log.warn("秒杀订单监听器:订单编号:{} 该订单已经消费过");
                 return CONSUME_SUCCESS;
             }
-            //业务幂等:判断同一个商品id +userPhone只有一个订单
 
+            //业务幂等:判断同一个商品id +userPhone只有一个订单
+            String productId = orderMsgProtocol.getProductId();
+            String userPhone = orderMsgProtocol.getUserPhone();
+            List<Order> businessList = orderService.list(new QueryWrapper<Order>()
+                    .lambda()
+                    .eq(Order::getProId, productId)
+                    .eq(Order::getUserPhone, userPhone));
+            if (CollectionUtil.isNotEmpty(businessList)) {
+                log.warn("秒杀订单监听器:用户编号:{},商品id:{},已经存在,不可重复下单", userPhone, productId);
+            }
+
+            //todo 执行扣减库存 下单操作
         }
         return null;
     }
