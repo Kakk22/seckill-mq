1941a8ef1f5a98d853a1f013ed3b92fe620eea88
Kakk22 <1029839308@qq.com>
2021-03-28 21:21:57 +0800

    压测脚本,真实扣减库存还要再调整一下

--- seckill-order-service/src/main/java/com/cyf/mq/listener/SeckillOrderLister.java
+++ seckill-order-service/src/main/java/com/cyf/mq/listener/SeckillOrderLister.java
@@ -88,10 +88,10 @@ public class SeckillOrderLister implements MessageListenerConcurrently {
             if (orderResult != null) {
                 log.info("用户:{},扣减商品id:{},下单成功!!!", userPhone, productId);
                 // 模拟订单处理，直接修改订单状态为处理中
-                Order updateOrder = new Order();
-                updateOrder.setStatus(OrderEnum.DOING.getStatus());
-                updateOrder.setId(orderResult.getId());
-                orderService.updateById(updateOrder);
+//                Order updateOrder = new Order();
+////                updateOrder.setStatus(OrderEnum.DOING.getStatus());
+////                updateOrder.setId(orderResult.getId());
+////                orderService.updateById(updateOrder);
 
                 return CONSUME_SUCCESS;
             }
